6ae4b2dfcd7b9fd6619123a99d2caed0edc2927b
Luis Alvarez <mrluisfeer@gmail.com>
2025-11-14 22:29:02 -0600

    fix: update lastUpdateCheck timestamp and improve footer styles

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,7 +2,7 @@ import FooterLinks from "./components/Links";
 
 export default function Footer() {
 	return (
-		<div className="absolute bottom-0 left-0 h-fit max-h-28 w-full bg-neutral-900 py-4 text-white">
+		<div className="absolute bottom-0 left-0 h-28 w-full bg-neutral-900 py-4 text-white">
 			<div className="mx-auto flex max-w-5xl flex-col justify-between gap-4 px-8 py-4 sm:flex-row sm:items-center sm:gap-0">
 				<div className="animate-pulse">Made with &#128153; by mrLuisFer</div>
 				<FooterLinks />
